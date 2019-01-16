cbf14625280c027ed1e648def8f8c491cd52c69c
Vinci2049 <vinci2049@gmail.com>
2019-01-17 02:19:01 +0400

    Ajax output on form

--- src/main/java/com/epam/rd/onlineStore/controller/ProductController.java
+++ src/main/java/com/epam/rd/onlineStore/controller/ProductController.java
@@ -47,6 +47,11 @@ public class ProductController {
         model.put("products", products);
         model.put("categories", categories);
 
+
+        Product product = new Product();
+        model.put("product", product);
+
+
         //model.put("time", new Date());
         //model.addAttribute("employees", manager.getAllEmployees());
         //return "employeesListDisplay";
@@ -60,20 +65,26 @@ public class ProductController {
 
 //    @RequestMapping(value = "/ajaxtest", method = RequestMethod.GET)
     @RequestMapping(value = "/products/{productId}/edit", method = RequestMethod.GET)
-    public @ResponseBody
-    String getTime(@PathVariable("productId") long productId) {
+    public //@ResponseBody
+    String getTime(Model model, @PathVariable("productId") long productId) {
 
 //        Random rand = new Random();
 //        float r = rand.nextFloat() * 100;
 //        String result = "<br>Next Random # is <b>" + r + "</b>. Generated on <b>" + new Date().toString() + "</b>";
 //        System.out.println("Debug Message from CrunchifySpringAjaxJQuery Controller.." + new Date().toString());
 
-        Product product = this.productService.findById(productId);
-        String result = product.getId()+", "+product.getName();
-        System.out.println("Debug Message from CrunchifySpringAjaxJQuery Controller.." + productId);
-        System.out.println("Debug Message from CrunchifySpringAjaxJQuery Controller.." + result);
 
-        return result;
+//        Product product = this.productService.findById(productId);
+//        String result = product.getId()+", "+product.getName();
+//        return result;
+
+
+        //Product product = this.productService.findById(productId);
+        model.addAttribute("product", this.productService.findById(productId));
+        //model.put("product", product);
+        return "createOrUpdateProductForm:: productEdit";
+        //return "createOrUpdateProductForm";
+
     }
 
 
